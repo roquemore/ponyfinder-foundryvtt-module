d9bff15b712ab0cfe443a5d10165d0a77570e56e
Memely <memelypepeartly@gmail.com>
2022-05-21 02:18:20 -0600

    update

--- .github/workflows/build-module-json.js
+++ .github/workflows/build-module-json.js
@@ -5,6 +5,6 @@ const version = JSON.parse(fs.readFileSync('module.json', 'utf8')).version;
 var mod = JSON.parse(fs.readFileSync('template.json', 'utf8'));
 
 mod.version = version;
-mod.download = `https://github.com/mrprimate/ddb-importer/releases/download/${version}/ponyfinder-foundryvtt-module.zip`;
+mod.download = `https://github.com/MemelyPepeartly/ponyfinder-foundryvtt-module/releases/download/${version}/ponyfinder-foundryvtt-module.zip`;
 
 console.log(JSON.stringify(mod));
