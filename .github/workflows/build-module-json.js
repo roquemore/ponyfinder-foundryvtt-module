654d9caa6af882bdc26410475a983886ad86524d
Memely <memelypepeartly@gmail.com>
2022-05-23 09:56:24 -0600

    templating

--- .github/workflows/build-module-json.js
+++ .github/workflows/build-module-json.js
@@ -5,6 +5,5 @@ const version = JSON.parse(fs.readFileSync('module.json', 'utf8')).version;
 var mod = JSON.parse(fs.readFileSync('template.json', 'utf8'));
 
 mod.version = version;
-mod.download = `https://github.com/MemelyPepeartly/ponyfinder-foundryvtt-module/releases/download/${version}/ponyfinder-foundryvtt-module.zip`;
 
 console.log(JSON.stringify(mod));
