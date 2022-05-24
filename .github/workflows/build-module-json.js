5ec22306930888d465d805f9e9afd8fbbc3ad82b
Memely <memelypepeartly@gmail.com>
2022-05-23 20:39:19 -0600

    dont really need these anymore

--- .github/workflows/build-module-json.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-const version = JSON.parse(fs.readFileSync('module.json', 'utf8')).version;
-
-var mod = JSON.parse(fs.readFileSync('template.json', 'utf8'));
-
-mod.version = version;
-
-console.log(JSON.stringify(mod));
